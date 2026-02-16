2da0ff5f1136be3769afc17cd989573b19530d38
Abhijeet Mishra <158300568+Abhijeet60078@users.noreply.github.com>
2026-02-16 15:48:47 +0530

    Delete Excercise3 directory

--- Excercise3/router/bookRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllBooks } from "../controller/bookController.js";
-
-const router = express.Router();
-
-router.get("/", getAllBooks);
-
-export default router;
